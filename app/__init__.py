588d521dd4cfcbdb115035d253184a58446b592b
John Njuguna <125473490+Uuuuu77@users.noreply.github.com>
2024-02-12 16:41:34 +0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, jsonify
 
-# Create a Flask application instance
-app = Flask(__name__)
+def create_app():
+    # Create a Flask application instance
+    app = Flask(__name__)
 
-# Import routes
-from app.routes import routes_bp
+    # Import routes inside the function to avoid circular imports
+    from app.routes import routes_bp
 
-# Register the Blueprint with the app
-app.register_blueprint(routes_bp)
+    # Register the Blueprint with the app
+    app.register_blueprint(routes_bp)
+
+    # Global error handler
+    @app.errorHandler(500)
+    def handle_500_error(e):
+        return jsonify(error=str(e)), 500
+
+    # You can include additional initialization code here if needed
+    # For example, connecting to a database, setting configurations, etc.
+
+    return app
+
+app = create_app()
 
 # You can include additional initialization code here if needed
 # For example, connecting to a database, setting configurations, etc.
