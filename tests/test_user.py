119d130f85449bb8720f0705ebc1bc6f6c26c3f1
John Njuguna <125473490+Uuuuu77@users.noreply.github.com>
2024-02-16 17:37:24 +0300

    Updates

---
+++ tests/test_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import unittest
+from models import User, db
+
+class UserTest(unittest.TestCase):
+    def test_create_user(self):
+        user = User(email='test@example.com', password='password')
+        db.session.add(user)
+        db.session.commit()
+
+        retrieved_user = User.query.filter_by(email='test@example.com').first()
+        self.assertIsNotNone(retrieved_user)
+        self.assertEqual(retrieved_user.email, 'test@example.com')
