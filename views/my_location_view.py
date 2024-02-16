10a795c2e3bb0bd93088827c31c2a92dd4a78e35
John Njuguna <johnmnjuguna89@gmail.com>
2024-02-16 05:10:53 -0800

    Updates

--- views/my_location_view.py
+++ views/my_location_view.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from flask import Blueprint, jsonify
-from services.my_location import get_my_location
+from services.my_location import get_location
 
 my_location_view = Blueprint('my_location_view', __name__)
 
