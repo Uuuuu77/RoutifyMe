f5cb11e8d78773412b144ddcb776ca08e48687ff
John Njuguna <johnmnjuguna89@gmail.com>
2024-03-20 02:30:15 -0700

    Updates

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def get_my_location_view():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
